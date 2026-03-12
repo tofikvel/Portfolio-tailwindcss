c90414cba86bdb0fd3fc0db53ad79505fb747118
Tofig Valiyev <tofig.valiyev1993@gmail.com>
2026-03-12 01:12:12 +0100

    fixed major layout bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,112 @@
 import Link from "next/link";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <section className="flex flex-col-reverse items-center gap-8 px-6 py-12 mx-auto md:flex-row md:justify-between max-w-6xl">
-        <div className="flex-1 space-y-8 md:space-y-12">
-          <h1 className="text-4xl font-bold md:text-5xl">
-            Transforming your <br />
-            IT Landscape
-          </h1>
-          <div className="space-y-12">
+      <section className="md:h-screen flex justify-center items-center gap-8 px-6 py-12 mx-auto">
+        <div className="flex-1 flex flex-col-reverse items-center gap-12 max-w-6xl md:space-y-12 md:flex md:flex-row">
+          <div className="flex-1 space-y-8 md:gap-12">
+            <h1 className="text-4xl font-bold md:text-5xl">
+              Transforming your <br />
+              IT Landscape
+            </h1>
             <p className="text-xl text-gray-600 leading-relaxed md:text-2xl">
               I’m a freelance <strong>Cloud</strong> and <strong>DevOps</strong> engineer helping companies design
               scalable, secure, and cost-efficient systems. Whether you are modernizing legacy applications or building
               cloud-native platforms, I can help you deliver robust solutions. Let’s elevate your infrastructure —
               contact me to discuss your needs.
             </p>
+            <div className="flex justify-center md:flex md:justify-start">
+              <Link
+                href="/portfolio"
+                className="bg-emerald-700 text-white px-6 py-4 rounded-full hover:bg-emerald-800 transition cursor-pointer md:px-12 md:py-4"
+              >
+                Get in touch!
+              </Link>
+            </div>
           </div>
-          <div className="flex justify-center md:flex md:justify-start">
-            <Link
-              href="/portfolio"
-              className="bg-emerald-700 text-white px-6 py-4 rounded-full hover:bg-emerald-800 transition cursor-pointer md:px-12 md:py-4"
-            >
-              Get in touch!
-            </Link>
+          <div className="flex-1 space-y-6">
+            <Image
+              src="/home-page-hero.jpg"
+              alt="home-page-hero-image"
+              width={680}
+              height={600}
+              className="w-full h-auto"
+            />
           </div>
-        </div>
-        <div className="flex-1 space-y-6 border border-black">
-          <Image src="/home-page-hero.jpg" alt="home-page-hero-image" width={680} height={600} className="w-full h-auto" />
         </div>
       </section>
 
-      <section className="flex flex-col items-center gap-8 px-6 py-20 bg-sky-100">
-        {/* Section title */}
-        <h1 className="mb-12 text-3xl font-bold text-center md:text-5xl md:mb-20">Services that deliver</h1>
-        {/* Services container */}
-        <div className="space-y-12 mx-auto max-w-3xl">
-          {/* Service 1 */}
-          <div className="pb-8 border-b border-gray-300">
-            <p className="mb-3 text-md text-gray-500 uppercase">Software development & integration</p>
-
-            <p className="text-xl font-semibold leading-relaxed md:text-3xl">
-              Custom software solutions and seamless integration with existing systems
-            </p>
+      <section className="md:h-screen flex justify-center items-center gap-8 px-6 py-12 mx-auto bg-sky-100">
+        <div className="flex-1 flex flex-col gap-12 max-w-6xl md:space-y-12">
+          {/* Section title */}
+          <div className="text-center">
+            <h1 className="text-3xl font-bold text-center md:text-5xl">Services that deliver</h1>
           </div>
-          <div className="pb-8 border-b border-gray-300">
-            <p className="mb-3 text-md text-gray-500 uppercase">Devops implementation & consulting</p>
+          <div className="flex flex-col gap-12">
+            {/* Service 1 */}
+            <div className="pb-8 border-b border-gray-300">
+              <p className="mb-3 text-md text-gray-500 uppercase">Software development & integration</p>
 
-            <p className="text-xl font-semibold leading-relaxed md:text-3xl">
-              Establish robust CI/CD pipelines, automating deployments and improving collaboration
-            </p>
-          </div>
-          <div className="pb-8 border-b border-gray-300">
-            <p className="mb-3 text-md text-gray-500 uppercase">Cloud migration & optimization</p>
+              <p className="text-xl font-semibold leading-relaxed md:text-3xl">
+                Custom software solutions and seamless integration with existing systems
+              </p>
+            </div>
+            <div className="pb-8 border-b border-gray-300">
+              <p className="mb-3 text-md text-gray-500 uppercase">Devops implementation & consulting</p>
 
-            <p className="text-xl font-semibold leading-relaxed md:text-3xl">
-              Seamless transition to the cloud, maximizing efficiency and cost savings
-            </p>
+              <p className="text-xl font-semibold leading-relaxed md:text-3xl">
+                Establish robust CI/CD pipelines, automating deployments and improving collaboration
+              </p>
+            </div>
+            <div className="pb-8 border-b border-gray-300">
+              <p className="mb-3 text-md text-gray-500 uppercase">Cloud migration & optimization</p>
+              <p className="text-xl font-semibold leading-relaxed md:text-3xl">
+                Seamless transition to the cloud, maximizing efficiency and cost savings
+              </p>
+            </div>
           </div>
         </div>
+        {/* Services container */}
       </section>
 
       {/* Project results section */}
-      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-24 mx-auto md:flex-row max-w-6xl">
-        <div className="flex-1 space-y-6 md:space-y-12">
-          <h1 className="text-3xl font-bold md:text-5xl">Proven project results</h1>
-          <div className="space-y-12">
-            <p className="text-xl text-gray-600 leading-relaxed md:text-2xl">
-              Explore a selection of my successfully completed projects, demonstrating my ability to deliver
-              high-quality, effective solutions across various industries and technologies. Each project showcases the
-              application of my skills to solve real-world challenges.
-            </p>
+      <section className="md:h-screen flex justify-center items-center gap-8 px-6 py-12 mx-auto">
+        <div className="flex-1 flex flex-col-reverse items-center gap-12 max-w-6xl md:space-y-12 md:flex md:flex-row">
+          <div className="flex-1 space-y-6 md:space-y-12">
+            <div className="flex-1 space-y-8 md:gap-12">
+              <h1 className="text-4xl font-bold md:text-5xl">Proven project results</h1>
+              <p className="text-xl text-gray-600 leading-relaxed md:text-2xl">
+                Explore a selection of my successfully completed projects, demonstrating my ability to deliver
+                high-quality, effective solutions across various industries and technologies. Each project showcases the
+                application of my skills to solve real-world challenges.
+              </p>
+              <div className="flex justify-center md:flex md:justify-start">
+                <Link
+                  href="/portfolio"
+                  className="bg-emerald-700 text-white px-6 py-4 rounded-full hover:bg-emerald-800 transition cursor-pointer md:px-12 md:py-4 uppercase"
+                >
+                  explore my projects
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center md:flex md:justify-start">
-            <Link
-              href="/portfolio"
-              className="bg-emerald-700 text-white px-6 py-4 rounded-full hover:bg-emerald-800 transition cursor-pointer md:px-12 md:py-4"
-            >
-              Explore my projects
-            </Link>
+          <div className="flex-1 space-y-6">
+            <Image
+              src="/home-page-hero.jpg"
+              alt="home-page-hero-image"
+              width={680}
+              height={600}
+              className="w-full h-auto"
+            />
           </div>
-        </div>
-        <div className="flex-1 space-y-6">
-          <Image src="/home-page-hero.jpg" alt="home-page-hero-image" width={680} height={600} className="w-full h-auto" />
         </div>
       </section>
 
       {/* Contact section */}
-      <section className="bg-sky-100 flex flex-col-reverse items-center justify-between gap-8 px-6 py-20 mx-auto md:flex-col">
-        <div className="flex-1md:space-y-24">
+      <section className="bg-sky-100 md:h-screen flex justify-center items-center gap-8 px-6 py-12 mx-auto">
+        <div className="flex-1 md:space-y-24 max-w-6xl">
           {/* TOP HEADING */}
           <div className="text-center mb-12">
             <div className="w-full md:flex md:justify-center">
@@ -97,7 +114,8 @@ export default function Home() {
             </div>
             <div className="w-full md:flex md:justify-center">
               <p className="text-gray-700 text-xl max-w-xl md:text-2xl leading-relaxed">
-                I would be happy to discuss your project with you. Please complete the form below or contact me directly.
+                I would be happy to discuss your project with you. Please complete the form below or contact me
+                directly.
               </p>
             </div>
           </div>
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,30 +1,47 @@
 export default function ServicesPage() {
   return (
-    <section className="flex flex-col md:h-screen bg-[url(/services-bg.jpg)] bg-cover bg-fixed bg-no-repeat bg-mist-900 bg-blend-overlay">
-      <div className="mx-auto flex max-w-6xl flex-1 flex-col px-6 gap-8 py-12 md:py-24">
+    <main>
+      <section className="flex flex-col md:h-screen bg-[url(/services-bg.jpg)] bg-cover bg-fixed bg-no-repeat bg-mist-900 bg-blend-overlay">
+        <div className="mx-auto flex max-w-6xl flex-1 flex-col px-6 gap-8 py-12 md:py-12">
+          <div className="flex flex-1 flex-col items-center justify-center text-center gap-6 md:gap-8">
+            <h1 className="text-3xl font-bold text-white md:text-7xl">IT and Cloud Consulting</h1>
+            <p className="text-md text-white md:text-3xl max-w-3xl">
+              Cloud solutions, CI/CD automation, and custom software development for scalable and secure businesses.
+            </p>
+          </div>
 
-        <div className="flex flex-1 flex-col items-center justify-center text-center gap-6 md:gap-8">
-          <h1 className="text-3xl font-bold text-white md:text-7xl">
-            IT and Cloud Consulting
-          </h1>
-          <p className="text-md text-white md:text-3xl max-w-3xl">
-            Cloud solutions, CI/CD automation, and custom software development for scalable and secure businesses.
-          </p>
+          <div className="flex justify-center">
+            <button className="cursor-pointer p-3">
+              <svg
+                className="w-8 h-8 text-white animate-bounce"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 9-7 7-7-7"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-
-        <div className="flex justify-center">
-          <button className="cursor-pointer p-3 border border-white">
-            <svg
-              className="w-6 h-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7" />
-            </svg>
-          </button>
+      </section>
+      <section className="flex flex-col md:h-screen">
+        <div className="mx-auto flex max-w-6xl flex-1 flex-col px-6 gap-8 py-12 md:py-24 border">
+          <div className="flex flex-1 flex-col items-center justify-center text-center gap-6 md:gap-12">
+            <h1 className="text-3xl font-bold md:text-5xl">Why Work With Me?</h1>
+            <p className="text-md text-gray-500 max-w-3xl md:text-2xl">
+              I combine deep technical expertise with a hands-on, results-driven approach to deliver real value for your
+              business.
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+        <div className="flex flex-row justify-between gap-6"></div>
+      </section>
+    </main>
+  );
 }
